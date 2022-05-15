4065bf33ca0cd9811849cfc292a4c3893be32c31
yana-michelle <60404658+yana-michelle@users.noreply.github.com>
2022-05-15 16:46:40 -0500

    why isn't this happening

--- java.js
+++ java.js
@@ -1,8 +1,58 @@
-const cards = document.querySelector('.card');
+const cards = document.querySelectorAll('.card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    if (lockBoard) return;
+    this.classList.add('flip');
+    
+    if (this === firstCard) return;
+
+    if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+   }
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    disableCards();
+    return;
+  }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    
+    resetBoard();
+}
+
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+   }, 1500);
+} 
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListner('click', flipCard));
+
 
