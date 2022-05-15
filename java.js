47db231a13752b8c5bd38ffefcfbebce3e6d2665
yana-michelle <60404658+yana-michelle@users.noreply.github.com>
2022-05-15 15:17:15 -0500

    changes

--- java.js
+++ java.js
@@ -1,8 +1,55 @@
-const button6 = document.getElementById('button6');
+const cards1 = document.querySelector('.card1');
 
-button6.addEventListener('click', () => {
-    button6.style.display = 'none' ;
+function flipCard() {
+    this.classList.toggle('flip');
+}
 
-    const btn6 = document.getElementById('btn6');
-    BaseAudioContext.style.display = 'block';
-});
+cards1.forEach(card => card.addEventListner('click', flipCard));
+
+const cards2 = document.querySelector('.card2');
+
+
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards2.forEach(card => card.addEventListner('click', flipCard));
+
+
+
+const cards3 = document.querySelector('.card3');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards3.forEach(card => card.addEventListner('click', flipCard));
+
+
+const cards4 = document.querySelector('.card4');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards4.forEach(card => card.addEventListner('click', flipCard));
+
+
+const cards5 = document.querySelector('.card5');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards5.forEach(card => card.addEventListner('click', flipCard));
+
+
+const cards6 = document.querySelector('.card6');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards6.forEach(card => card.addEventListner('click', flipCard));
+
