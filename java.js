7fe35bcc61f2fd2a3abc91237d73d4265f4ae498
yana-michelle <60404658+yana-michelle@users.noreply.github.com>
2022-05-15 14:33:15 -0500

    redo

--- java.js
+++ java.js
@@ -1,4 +1,8 @@
-function setImageVisible(button6, visible){
-    var img = document.getElementById(button6);
-    img.style.visibility = (visible ? 'visible' : 'hidden');
-}
+const button6 = document.getElementById('button6');
+
+button6.addEventListener('click', () => {
+    button6.style.display = 'none' ;
+
+    const btn6 = document.getElementById('btn6');
+    BaseAudioContext.style.display = 'block';
+});
